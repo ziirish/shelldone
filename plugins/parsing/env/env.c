ea831d9002619ca8647ecb2df79d5f4b123bcdf5
Ziirish <sans.benjamin@gmail.com>
2012-05-13 18:00:46 +0200

    add: recursive setting (ie. toto=$tata)

--- plugins/parsing/env/env.c
+++ plugins/parsing/env/env.c
@@ -31,6 +31,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#ifndef _BSD_SOURCE
+    #define _BSD_SOURCE
+#endif
 #ifndef _XOPEN_SOURCE
     #define _XOPEN_SOURCE
 #endif
@@ -38,11 +41,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <wordexp.h>
 
 #include <sdlib/plugin.h>
 #include <xutils.h>
 #include <structs.h>
-#include "list.h"
+#include <list.h>
+#include <modules.h>
 
 static sdlist *env;
 
@@ -79,7 +84,68 @@ sd_putenv (const char *set)
 {
     sddata *tmp = xmalloc (sizeof (*tmp));
     tmp->content = xstrdup (set);
+    if (is_module_present ("wildcards"))
+    {
+        wordexp_t p;
+        int r, i;
+        char *param = strstr (tmp->content, "=");
+        char *ini = xstrsub (tmp->content,
+                             0,
+                             (int)(xstrlen (tmp->content) - xstrlen (param)));
+        ++param;
+/*        fprintf (stdout, "working on %s, ini: %s\n", param, ini);*/
+        r = wordexp (param, &p, 0);
+        if (p.we_wordc == 0 || (p.we_wordc == 1 &&
+            xstrcmp (p.we_wordv[0], param) == 0))
+        {
+            if (r == 0)
+                wordfree (&p);
+
+            sd_info ("'%s' is not set, using empty value\n", param);
+            xfree (tmp->content);
+            xfree (ini);
+            xfree (tmp);
+
+            return 0;
+        }
+        else if (r == 0)
+        {
+            if (p.we_wordc > 1)
+            {
+                char *res = NULL;
+                size_t l = 0;
+                for (i = 0; i < (int) p.we_wordc; i++)
+                    l += xstrlen (p.we_wordv[i]) + 1;
+                res = xmalloc (l * sizeof (char));
+                memset (res, 0, l);
+                for (i = 0; i < (int) p.we_wordc; i++)
+                    snprintf (res,
+                              xstrlen (res) + xstrlen (p.we_wordv[i]) + 1,
+                              "%s%s%c",
+                              res,
+                              p.we_wordv[i],
+                              i == (int) p.we_wordc ? '\0' : ' ');
+
+                wordfree (&p);
+                xfree (tmp->content);
+                l = xstrlen (ini) + xstrlen (res) + 2;
+                tmp->content = xmalloc (l * sizeof (char));
+                snprintf (tmp->content, l, "%s=%s", ini, res);
+                xfree (res);
+            }
+            else
+            {
+                size_t l = xstrlen (ini) + xstrlen (p.we_wordv[0]) + 2;
+                xfree (tmp->content);
+                tmp->content = xmalloc (l * sizeof (char));
+                snprintf (tmp->content, l, "%s=%s", ini, p.we_wordv[0]);
+                wordfree (&p);
+            }
+        }
+        xfree (ini);
+    }
     list_append (&env, tmp);
+
     return putenv (tmp->content);
 }
 
@@ -100,7 +166,7 @@ sd_plugin_main (void **data)
     command **tmpc = (command **)tmp;
     command *cmd = *tmpc;
     if (cmd->cmd != NULL &&
-        (cmd->protect & SETTING) != 0 && cmd->argc < 1)
+        (cmd->protect & SETTING) && cmd->argc < 1)
     {
         sd_debug ("setting: '%s'\n", cmd->cmd);
         sd_putenv (cmd->cmd);
@@ -109,7 +175,7 @@ sd_plugin_main (void **data)
     }
     if (cmd->argc > 0)
     {
-        while ((cmd->protect & SETTING) != 0 && cmd->argc > 0)
+        while ((cmd->protect & SETTING) && cmd->argc > 0)
         {
             int i;
             char **argv = xcalloc (cmd->argc-1, sizeof (char *));
@@ -130,7 +196,7 @@ sd_plugin_main (void **data)
             cmd->protected = prot;
             (cmd->argc)--;
         }
-        if ((cmd->protect & SETTING) != 0 && cmd->argc == 0)
+        if ((cmd->protect & SETTING) && cmd->argc == 0)
         {
             sd_debug ("setting: '%s'\n", cmd->cmd);
             sd_putenv (cmd->cmd);
@@ -151,7 +217,7 @@ sd_plugin_main (void **data)
                 cmd->argvf = xrealloc (cmd->argvf, cmd->argc * sizeof(char *));
                 cmd->argcf = cmd->argc;
             }
-            if ((cmd->protected[i] & SETTING) != 0)
+            if (cmd->protected[i] & SETTING)
             {
                 sd_debug ("setting: '%s'\n", cmd->argv[i]);
                 sd_putenv (cmd->argv[i]);
